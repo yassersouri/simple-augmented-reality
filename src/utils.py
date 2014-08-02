52c8e3f02420dc5365a56ce3bd85bbdc6afaf157
yassersouri <yassersouri@gmail.com>
2014-08-03 02:19:29 +0430

    clean up, still refining in realtime does not work

--- src/utils.py
+++ src/utils.py
@@ -23,17 +23,52 @@ color5 = (255, 0, 0)
 COLOR = [color1, color2, color3, color4, color5]
 assert len(COLOR) == N
 
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+global points
+
+def pointsToCorner(points):
+    corner = np.zeros((len(points), 1, 2), dtype=np.float32)
+    
+    for p, i in zip(points, range(len(points))):
+        corner[i, 0, :] = [p[0], p[1]]
+    
+    return corner
+
+
+def cornerToPoints(corner):
+    points = []
+    
+    for i in range(corner.shape[0]):
+        points.append((corner[i, 0, 0], corner[i, 0, 1]))
+    
+    return points
+
+
+def refinePoints(gray):
+    global points
+    
+    corner = pointsToCorner(points)
+    cv2.cornerSubPix(gray, corner, (11, 11), (-1, -1), criteria)
+    points = cornerToPoints(corner)
+
+
 def getPoints(img, COLLECTION_NUM, IMAGE_NUM):
+    global points
+    
     imgO = img
+    gray = cv2.cvtColor(imgO, cv2.COLOR_BGR2GRAY)
     
     PTS_COLLECTION_PATH = os.path.join(POINTS_FOLDER, str(COLLECTION_NUM))
     pts_file_name = os.path.join(PTS_COLLECTION_PATH, str(IMAGE_NUM) + PTS_FILE_EXTENSION)
     files = glob.glob(pts_file_name)
 
-    points = []
     WINDOW_NAME = "get points"
+    points = []
     
-    def draw_points(imgO, points, radius=4, thickness=2):
+    def draw_points(imgO, radius=4, thickness=2):
+        global points
+        
         imgD = np.copy(imgO)
         i = 0
         for point in points:
@@ -42,18 +77,24 @@ def getPoints(img, COLLECTION_NUM, IMAGE_NUM):
         return imgD
     
     def onMouse(event, x, y, _, D):
+        global points
+        
         if event == cv2.EVENT_LBUTTONDOWN:
             if len(points) == M * N:
                 print 'FULL'
                 return
             points.append((x, y))
-            imgD = draw_points(imgO, points)
+            imgD = draw_points(imgO)
             cv2.imshow(WINDOW_NAME, imgD)
     
-    def GUI(points):
+    def GUI():
+        global points
+        
         done = False
         while(True):
-            imgD = draw_points(imgO, points)
+            if len(points) > 0:
+                refinePoints(gray)
+            imgD = draw_points(imgO)
             cv2.imshow(WINDOW_NAME, imgD)
             cv2.setMouseCallback(WINDOW_NAME, onMouse)
             pressedChar = cv2.waitKey(0)
@@ -68,34 +109,28 @@ def getPoints(img, COLLECTION_NUM, IMAGE_NUM):
                 #pressed U
                 if len(points) > 0:
                     points.pop()
+                    print 'popped'
+                    
             else:
-                print 'Press these:'
+                print 'Press one of these:'
                 print '\tESC:\t\t to exit'
                 print '\tENTER:\t\t to proceed'
                 print '\tU:\t\t to remove poins'
         return done
     
-    if len(files) == 0:
-        #try to get the points from user GUI.
-        done = GUI(points)
-        
-        if done:
-            #save points
-            pts = np.array(points)
-            np.save(pts_file_name, pts)
-        
-        return done, points
-                
-    else:
+    if len(files) == 1:
+        # if file exists load from it
         pts = np.load(files[0])
         points = pts.tolist()
-        done = GUI(points)
-        
-        if done:
-            pts = np.array(points)
-            np.save(pts_file_name, pts)
-        
-        return done, points
+    
+    # show the gui and let the user edit the points
+    done = GUI()
+    if done:
+        #save points
+        pts = np.array(points)
+        np.save(pts_file_name, pts)
+    
+    return done, points
 
 
 def getCollectionPhotos(collectionNum, scale_down_factor=1):
@@ -112,53 +147,6 @@ def getCollectionPhotos(collectionNum, scale_down_factor=1):
     
     return imgs
 
-def draw(img, corners, imgpts):
-    corner = tuple(corners[0].ravel())
-    cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
-    cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
-    cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
-    return img
-    
-
-def findChessBoardPoints(img):
-    
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    
-    M, N = 6, 7
-    
-    objp = np.zeros((M*N, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:N, 0:M].T.reshape(-1, 2)
-    
-    axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]])
-    #axis = axis.reshape(-1, 3)
-    
-    objpoints = []
-    imgpoints = []
-    
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    ret, corners = cv2.findChessboardCorners(gray, (N, M), None, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_FILTER_QUADS)
-    
-    print ret
-    
-    if ret == True:
-        objpoints.append(objp)
-        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners)
-        
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        
-        rvecs2, tvecs2, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)
-        
-        imgpts, jac = cv2.projectPoints(axis, np.array(rvecs), np.array(tvecs), mtx, dist)
-        
-        img = draw(img, corners, imgpts)
-        
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     img1, img2, img3 = getCollectionPhotos(100)
-    findChessBoardPoints(img2)
